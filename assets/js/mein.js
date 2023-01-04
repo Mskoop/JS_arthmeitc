bbfbc6435e428d1f5d61090280f4ff2c06325f3b
Msqoop <supercoder@supercoders-air.speedport.ip>
2023-01-04 01:37:08 +0100

    auf_1.12_1.13

---
+++ assets/js/mein.js
@@ -0,0 +1,55 @@
+// Aufgabe 1.12
+
+{let score = 5 + 5 * 10;
+console.log ('Ergebnis ' + score); }
+
+{let score = (5 + 5) * 10;
+console.log('Ergebnis ' + score);}
+
+{let score = 0;
+score = score + 10;
+console.log('Ergebnis ' + score);}
+
+/* 
+Kurzform:
+{ let score = 0;
+score+ = 10;
+console.log('Ergebnis ' + score);}  */
+
+
+
+{let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;}
+
+// Verwende Inkrement-Operatoren
+/* zahl++;
+console.log('increment: ' + zahl);
+
+// Verwende Dekrement-Operatoren
+zahl--;
+console.log('dekrement: ' + zahl);
+ */
+// Autgabe 1.13
+
+// Verwende Grundrechenarten
+
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
